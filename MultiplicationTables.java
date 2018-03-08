54b8e90636f00d98b3adcc08696c7614840a57a9
JESSINA L MCMULLEN <35549421+Sinakoba1@users.noreply.github.com>
2018-03-07 22:25:42 -0600

    Add files via upload

--- MultiplicationTables.java
+++ MultiplicationTables.java
@@ -13,14 +13,24 @@ public class MultiplicationTables
         out.print("How large would you like to see them? ");
         size = keyboard.nextInt();
 
+
         for (a = 0; a <= size; a++)
         {
-            if (a < 20)
+            if (a < 10)
+            {
                 out.print("");
+            }
             for (b = 0; b <= size; b++)
             {
                 if(size < 20)
+                {
                     out.println(a + "*" + b + "=" + a*b);
+                }
+                 else if (size > 20 || size < 1)
+                {
+                    out.println("Please choose a number between 1-20.");
+                    size = keyboard.nextInt();
+                }
             }out.print("");
         }
     }
